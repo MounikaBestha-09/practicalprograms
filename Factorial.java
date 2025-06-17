434844f5d464f28e4d371f1ae0cf0273299ced09
Mounika Bestha <22701A05C6@aitsrajampet.ac.in>
2025-06-17 22:35:04 +0530

    Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Factorial{
+    public static void main(String[] args){
+        int n,fact=1;
+        Scanner s=new Scanner(System.in);
+        System.out.println("enter a number");
+        n=s.nextInt();
+        for(int i=1;i<=n;i++){
+            fact=fact*i;
+        }
+        System.out.println("the factorial of "+n+" is "+fact);
+   }
+}
